81d0b2fdcd00001389bb7c013f985f1329b59987
Simon Green <simon@captaincodeman.com>
2023-02-03 09:31:55 -0700

    update pan-zoom

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,24 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
-const prod = process.env.NODE_ENV === "production";
+const prod = process.env.NODE_ENV === 'production'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: [
-    vitePreprocess(),
-    preprocess({
-      postcss: true,
-    }),
-  ],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess()],
 
-  kit: {
-    adapter: adapter({
-      pages: "docs",
-      assets: "docs",
-      fallback: null,
-    }),
-    paths: {
-      base: prod ? "/svelte-image-comparison" : "",
-    },
-  },
-};
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: null,
+		}),
+		paths: {
+			base: prod ? '/svelte-image-comparison' : '',
+		},
+	},
+}
 
-export default config;
+export default config
