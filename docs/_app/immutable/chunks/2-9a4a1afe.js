2f970fe1596fe74caecdf46c62a1f155912dde0f
Simon Green <simon@captaincodeman.com>
2023-06-22 10:14:50 -0600

    use github action for docs

--- docs/_app/immutable/chunks/2-9a4a1afe.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-dad0f70b.js";export{t as component};
--- docs/_app/immutable/chunks/3-1e194dd6.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/panzoom/_page.svelte-ce374bc3.js";export{t as component};
